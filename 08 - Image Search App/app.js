3dbe0e6dab33147876490c3cb40783b17a8014d4
Aman Gupta <amangupta6121998@gmail.com>
2022-10-15 08:11:18 +0530

    feat: add lodaer, operation function and updated comment convention

--- 08 - Image Search App/app.js
+++ 08 - Image Search App/app.js
@@ -9,6 +9,7 @@ const ItemsInNavBar = document.querySelectorAll(".Item");
 const SuggestionCardSection = document.getElementById("search-suggestion-card");
 const ResultNotFoundModal = document.getElementById("result-not-found");
 const SomethingWentWrongModal = document.getElementById("something-went-wrong");
+const Loader = document.getElementById("loader");
 
 // Total number of images to show in UI after API response.
 const NumberOfImagesPerPage = 50;
@@ -19,17 +20,19 @@ const ConfigureRequest = (SearchQuery) => {
 };
 
 const ClearPreviousSearchResult = () => {
-    SuggestionCardSection.style.display = "none";
     const hasImagePlaceHolder = document.getElementById("images-placeholder");
+
     if (hasImagePlaceHolder) {
-        hasImagePlaceHolder.remove();
+        hasImagePlaceHolder.remove(); // Remove all search result images in UI.
     }
-    //if any error screen present
-    RemoveErrorBox();
+
+    RemoveSearchSuggestions(); // Remove search related suggestion section.
+
+    RemoveErrorBox(); // Remove if any error screen present.
 };
 
 const CheckActiveItemInNav = (clickedNavItem) => {
-    const NavItems = ["All", "Image", "Videos", "News", "Books", "More"];
+    const NavItems = ["All", "Image", "Videos", "News", "Books", "More"]; // Items present in nav bar.
     NavItems.map((NavItem, index) => {
         if (NavItem === clickedNavItem) {
             ItemsInNavBar[index].classList.add("AciveItem");
@@ -40,27 +43,29 @@ const CheckActiveItemInNav = (clickedNavItem) => {
 };
 
 const AppendImagesInUI = (Images) => {
-    //create a placeholder to hold all images
+    // Create a placeholder to hold all images.
     const DivElement = document.createElement("div");
     DivElement.setAttribute("id", "images-placeholder");
     DivElement.classList.add("SearchImagesPlaceholder");
 
-    Images?.map((image) => {
-        const FetchedImageSource = image?.urls?.regular; //feteched image source form API
+    Images?.map((image, index) => {
+        const FetchedImageSource = image?.urls?.regular; // Feteched image source form API.
 
-        //Crad to hold a single image
+        // Card to hold a single image.
         const CardElement = document.createElement("div");
-        CardElement.setAttribute("id", "search-image-card");
+        CardElement.setAttribute("id", `search-image-card-${index}`);
         CardElement.classList.add("SearchImageCard");
 
-        //create a img element
+        // Create an img element.
         const ImageElement = document.createElement("img");
         ImageElement.setAttribute("src", FetchedImageSource);
 
-        CardElement.appendChild(ImageElement); //appending img element inside the image card
-        DivElement.appendChild(CardElement); // appending image card inside the image placeholder
-        SearchResultBox.appendChild(DivElement); //appending image placeholder inside the SearchResultBox
+        CardElement.appendChild(ImageElement); // Appending img element inside the image card.
+        DivElement.appendChild(CardElement); // Appending image card inside the image placeholder.
+        SearchResultBox.appendChild(DivElement); // Appending image placeholder inside the SearchResultBox.
     });
+
+    RemoveLoader();
 };
 
 const ShowErrorBox = (ErrorCode) => {
@@ -71,6 +76,7 @@ const ShowErrorBox = (ErrorCode) => {
     if (ErrorCode === 5000) {
         SomethingWentWrongModal.style.display = "grid";
     }
+    RemoveLoader();
 };
 
 const RemoveErrorBox = () => {
@@ -79,43 +85,54 @@ const RemoveErrorBox = () => {
     SomethingWentWrongModal.style.display = "none";
 };
 
+const RemoveLoader = () => {
+    Loader.style.display = "none";
+};
+
+const AddLoader = () => {
+    Loader.style.display = "grid";
+};
+
+const RemoveSearchSuggestions = () => {
+    SuggestionCardSection.style.display = "none";
+};
+
+const AddSearchSuggestions = () => {
+    SuggestionCardSection.style.display = "flex";
+};
+
 const FetchUserQuery = (UserQuery) => {
     fetch(ConfigureRequest(UserQuery))
         .then((response) => response.json())
         .then((fetechedQuery) => {
             const QueryResponse = fetechedQuery?.results;
+
             if (QueryResponse.length > 0) {
-                SuggestionCardSection.style.display = "flex";
+                AddSearchSuggestions();
                 AppendImagesInUI(QueryResponse);
             } else {
-                const ErrorCode = 4000; // status code for search result not found
+                const ErrorCode = 4000; // Status code for search result not found.
                 ShowErrorBox(ErrorCode);
             }
         })
         .catch((err) => {
-            const ErrorCode = 5000; // status code for fetch data failed
-            ShowErrorBox(ErrorCode);
+            const ErrorCode = 5000; // Status code for failed to fetch data from server.
+            setTimeout(() => {
+                ShowErrorBox(ErrorCode);
+            }, 5000);
         });
 };
 
 const InitiateUserQuerySearch = (UserQuery) => {
     if (UserQuery !== "") {
         ClearPreviousSearchResult();
-        // ErrorTextBox.innerText =
-        //     "Wait 🔄 Fetching your Result It will display here 👇  !!!";
+        CheckActiveItemInNav("Image"); // Add image as default active item in nav.
+        AddLoader();
         FetchUserQuery(UserQuery);
-    } else {
-        alert("Enter a Valid Input");
     }
 };
 
-//Add default active item in nav
-(() => {
-    CheckActiveItemInNav("Image");
-})();
-
 // Add event listeners.
-
 SearchButton.addEventListener("click", () => {
     const UserQuery = InputField.value;
     InitiateUserQuerySearch(UserQuery);
@@ -131,7 +148,7 @@ InputField.addEventListener("keypress", (e) => {
     }
 });
 
-//Add event listeners to each items in nav
+// Add event listeners to each items in nav.
 for (let i = 0; i < ItemsInNavBar.length; i++) {
     ItemsInNavBar[i].addEventListener("click", () => {
         const ClickedElement = ItemsInNavBar[i].className.split(" ");
