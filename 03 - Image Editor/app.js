4df8c81ef0a667f32029c07dbbc2ddc301109584
Aman Gupta <amangupta6121998@gmail.com>
2023-01-23 00:51:20 +0530

    refactor(app.js): add file accepted file formate

--- 03 - Image Editor/app.js
+++ 03 - Image Editor/app.js
@@ -1,20 +1,44 @@
+// Targeted Elements
+const DragArea = document.querySelector(".drag-area");
+const BrowseFileButton = document.getElementById("browse-file");
+const FilePicker = document.getElementById("file-picker");
+const store = document.querySelectorAll(".controls input");
 
-    
-    const store = document.querySelectorAll('.controls input');
+// Accepted file type
+const AcceptedFileType = ["image/png", "image/jpeg"];
 
-    function edit()
-    {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+const fileSupportError =
+    "Error: This file format is not supported. Please upload a file in a supported format.";
 
-    //event listeners
-    store.forEach(input =>
-    input.addEventListener('change',edit) 
+function edit() {
+    const suffix = this.dataset.sizing || "";
+    document.documentElement.style.setProperty(
+        `--${this.name}`,
+        this.value + suffix
     );
+}
+const handleImageUpload = (file) => {
+    console.log(file);
+};
+const handleUnSupportedFile = (error) => {
+    alert(error);
+};
 
-    store.forEach(input =>
-    input.addEventListener('mousemove', edit) 
-    );
-  
- 
+//event listeners
+store.forEach((input) => input.addEventListener("change", edit));
+
+store.forEach((input) => input.addEventListener("mousemove", edit));
+
+BrowseFileButton.addEventListener("click", () => {
+    FilePicker.click();
+});
+
+FilePicker.addEventListener("change", () => {
+    const file = FilePicker.files[0];
+
+    console.log(file);
+
+    file && AcceptedFileType.includes(file.type)
+        ? handleImageUpload(file)
+        : handleUnSupportedFile(fileSupportError);
+});
